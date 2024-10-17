aba0ad5246673a666053df023c4043e6a23feab4
José Felipe Holanda <josefelipehol@gmail.com>
2024-10-16 23:57:17 -0300

    fix: changed name to Func, fixed comp argument order and defined flip and dcomp

--- haskell/Comp.hs
+++
@@ -1,7 +0,0 @@
-module Comp where
-
-comp :: (a -> b) -> (b -> c) -> a -> c
-comp f g x = g (f x)
-
-(°) :: (b -> c) -> (a -> b) -> (a -> c)
-f ° g = comp g f
---
+++ haskell/Func.hs
@@ -0,0 +1,15 @@
+module Func where
+
+import Prelude hiding (flip)
+
+comp :: (b -> c) -> (a -> b) -> a -> c
+comp f g x = f (g x)
+
+(°) :: (b -> c) -> (a -> b) -> (a -> c)
+(°) = comp
+
+flip :: (a -> b -> c) -> b -> a -> c
+flip f x y = f y x
+
+dcomp :: (a -> b) -> (b -> c) -> a -> c
+dcomp = flip comp
