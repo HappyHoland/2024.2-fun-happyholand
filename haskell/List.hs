96e3d3ad0b7d58e789a41f0e692a38c7e703bab1
José Felipe Holanda <josefelipehol@gmail.com>
2024-10-18 15:05:20 -0300

    feat: defined map (and got rid of scalarOps) and changed constructors names

--- haskell/List.hs
+++ haskell/List.hs
@@ -3,56 +3,51 @@ module List where
 import Prelude hiding 
     ( Num(..), max, min, (^), Bool(..), (==), (<=),
      quot, rem, gcd, lcm, div, even, List(..),
-      length, sum, product, concat, repeat)
+      length, sum, product, concat, repeat, map)
 import Nat
 import Typeclasses
 
 data List a where
-    Empty :: List a
-    Cons :: a -> List a -> List a
+    Nil :: List a
+    (:>) :: a -> List a -> List a
     deriving (Eq, Show)
 
 length :: List a -> Nat
-length Empty = O
-length (Cons x xs) = S (length xs)
+length Nil = O
+length (x :> xs) = S (length xs)
 
 index :: List a -> Nat -> a
-index Empty n = error "index out of range"
-index (Cons x xs) O = x
-index (Cons x xs) (S n) = index xs n
+index Nil n = error "index out of range"
+index (x :> xs) O = x
+index (x :> xs) (S n) = index xs n
 
 concat :: List a -> List a -> List a
-concat Empty l = l
-concat (Cons x xs) l = Cons x (concat xs l)
+concat Nil l = l
+concat (x :> xs) l = x :> concat xs l
 
 repeat :: Nat -> a -> List a
-repeat O x = Empty
-repeat (S n) x = Cons x (repeat n x)
+repeat O x = Nil
+repeat (S n) x = x :> repeat n x
 
 stretch :: Nat -> List a -> List a
-stretch _ Empty = Empty
-stretch n (Cons x xs) = concat (repeat n x) (stretch n xs)
+stretch _ Nil = Nil
+stretch n (x :> xs) = concat (repeat n x) (stretch n xs)
 
 sum :: Additive a => List a -> a
-sum Empty = zero
-sum (Cons x xs) = x + sum xs
+sum Nil = zero
+sum (x :> xs) = x + sum xs
 
 product :: Multiplicative a => List a -> a
-product Empty = one
-product (Cons x xs) = x * product xs
+product Nil = one
+product (x :> xs) = x * product xs
 
-scalarOpL :: (a -> a -> a) -> a -> List a -> List a
-scalarOpL op x Empty = Empty
-scalarOpL op x (Cons y ys) = Cons (op x y) (scalarOpL op x ys) 
+map :: (a -> b) -> List a -> List b
+map f Nil = Nil
+map f (x :> xs) = f x :> map f xs
 
-scalarOpR :: (a -> a -> a) -> a -> List a -> List a
-scalarOpR op x Empty = Empty
-scalarOpR op x (Cons y ys) = Cons (op y x) (scalarOpR op x ys) 
-
-pwOp :: (a -> a -> a) -> List a -> List a -> List a
-pwOp op _ Empty = Empty
-pwOp op Empty _ = Empty
-pwOp op (Cons x xs) (Cons y ys) = Cons (op x y) (pwOp op xs ys)
-
+pwOp :: (a -> b -> c) -> List a -> List b -> List c
+pwOp op _ Nil = Nil
+pwOp op Nil _ = Nil
+pwOp op (x :> xs) (y :> ys) = op x y :> pwOp op xs ys
 
 
