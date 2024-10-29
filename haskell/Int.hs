e08db5bae9b9b4b50a692e2d90b1b6da85eccfd4
José Felipe Holanda <josefelipehol@gmail.com>
2024-10-29 11:58:38 -0300

    fix: changed sign indicator of int to Bool type

--- haskell/Int.hs
+++ haskell/Int.hs
@@ -7,13 +7,12 @@ import Typeclasses
 import Nat
 
 data Int where
-    Int :: (Nat, Nat) -> Int
-
+    Int :: (Bool, Nat) -> Int
 
 inv :: Int -> Int
 inv (Int (n, m))
-    | n == O = Int (S O, m)
-    | otherwise = Int (O, m)
+    | not n = Int (True, m)
+    | otherwise = Int (False, m)
 
 iszero :: Int -> Bool
 iszero (Int (_, O)) = True
@@ -23,28 +22,23 @@ instance Eq Int where
     Int (n, o) == Int (m, p)
         | o == O && p == O = True
         | n == m && o == p = True
-        | n >= S O && m >= S O && o == p = True
         | otherwise = False
 
 instance Show Int where
     show (Int (_, O)) = show O
-    show (Int (O, n)) = show n
-    show (Int (S n, m)) = "-" ++ show m 
+    show (Int (True, n)) = show n
+    show (Int (False, m)) = "-" ++ show m 
 
 instance Additive Int where
     Int (s, n) + Int (r, m)
-        | iszero (Int (r,m)) = Int (s, n)
         | s == r = Int (s, n + m)
         | n <= m = Int (r, abs n m)
         | otherwise = Int (s, abs n m)
     
-    zero = Int (O, O)
+    zero = Int (True, O)
 
 instance Multiplicative Int where
-    Int (s, n) * Int (r, m)
-        | iszero (Int (r,m)) = zero
-        | s == r = Int (O, n * m)
-        | otherwise = Int (S O, n * m)
+    Int (s, n) * Int (s', m) = Int (s == s', n * m)
 
-    one = Int (O, S O)
+    one = Int (True, S O)
 
